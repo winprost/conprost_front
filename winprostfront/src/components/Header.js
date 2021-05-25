303d2bc65f62518921522847a26fe577ef5ebef2
Kyilee <kyileeeyu@gmail.com>
2021-05-25 15:07:08 +0900

    Add : header

--- winprostfront/src/components/Header.js
+++ winprostfront/src/components/Header.js
@@ -1,43 +1,80 @@
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button,Card, Accordion, Dropdown,DropdownButton} from 'react-bootstrap';
 import React from 'react'
 import logo from '../images/logo.png';
+import styled from 'styled-components';
+
+let navbar = styled.div`
+    margin-left:30px;
+    padding-left : 30px;
+    justify-content:inherit;
+`;
+
+let search = styled.div`
+    border:none;
+
+
+`;
+
+let menu = styled.div`
+    margin-left : 100px;
+`;
+
 const Header = () => {
 
 return (
-<header classNameName='header'>
-    <div>
-        <div className="row top">
-        <div className="col-9"></div>
-        <div className="col-1">회원가입</div>
-        <div className="col-1">문제신고</div>
-        <div className="col-1">알림</div>
-    </div>
-    <hr/>
+    
+<div className="container">
     <div className="row">
-        <div className="nav">
-            <div className="col-3">
-                <a href="/#" className="logo"> 
-                    <img src={logo} alt="logo" width="100%"/> 
-                </a>
-            </div>
-            <div className="col-6">
-                <form method="GET" action="/project" name="projectIdx">
-                    <input type="text" placeholder="search" className="search" />
-                </form>
-            </div>
-            <div className="col-1">
-                <a href="/#">About</a>
-            </div>
-            <div className="col-1">
-                <a href="/#">Login</a>
-            </div>
-            <div className="col-1">
-                <a href="/#">Mypage</a>
-            </div>
+        <div className="col-12">
+            <Nav className="justify-content-end" activeKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">회원가입</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">문제신고</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">알림</Nav.Link>
+                </Nav.Item>
+            </Nav>
         </div>
     </div>
-    <hr/>
+    <div className="row">
+        <div className="col-12">
+            <navbar>
+                <Navbar bg="white" variant="light">
+
+                <DropdownButton id="dropdown-basic-button" title="Dr">
+                    <Dropdown.Item href="#/action-1">Contents
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Users</Dropdown.Item>
+                </DropdownButton>
+
+                <Navbar.Brand href="#home" className="logo">
+                    <img src={logo} alt="logo" width="150px"/> 
+                </Navbar.Brand>
+                <search>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-primary">Search</Button>
+                    </Form>
+                </search>
+                
+                <menu>
+                    <Nav className="justify-content-end" activeKey="/home">
+                        <Nav.Link href="#home">Team</Nav.Link>
+                        <Nav.Link href="#features">Mypage</Nav.Link>
+                        <Nav.Link href="#pricing">Login</Nav.Link>
+                    </Nav>
+                </menu>
+                </Navbar> 
+
+
+            </navbar>
+
+        </div> 
     </div>
-</header>
+</div>
 )
 }
 export default Header
