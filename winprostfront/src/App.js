5a85237d771f9ff0bccd5d15991d098e5c3c71df
Kyilee <kyileeeyu@gmail.com>
2021-06-16 20:52:49 +0900

    Modify: 다른 파일들 삭제

--- winprostfront/src/App.js
+++ winprostfront/src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Main from './components/Main'
-import TeamBoard from './pages/teamBoard'
-import ContestDetail from './pages/contestDetail'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
@@ -14,8 +12,6 @@ function App() {
     <div className="App">
       <Header />
       <Main /> 
-      <TeamBoard/>
-      <ContestDetail/>
       <Footer />
     </div>
   );
