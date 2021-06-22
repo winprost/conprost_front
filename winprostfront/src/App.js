12dfdfab433fe592849f9605bf408506ad016014
Kyilee <kyileeeyu@gmail.com>
2021-06-22 17:39:23 +0900

    Add: contestDetail, make contest detail section

--- winprostfront/src/App.js
+++ winprostfront/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Main from './components/Main'
+import ContestDetail from './components/contestDetail'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Main /> 
       <Footer />
+      <ContestDetail />
     </div>
   );
 }
