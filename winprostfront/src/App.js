347366c1b31393ea9382763d7e300af7508d2296
Kyilee <kyileeeyu@gmail.com>
2021-06-22 15:02:54 +0900

    Add: teamBoard page, make notice & team member list.

--- winprostfront/src/App.js
+++ winprostfront/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Main from './components/Main'
+import TeamBoard from './components/teamBoard'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Main /> 
       <Footer />
+      <TeamBoard />
     </div>
   );
 }
